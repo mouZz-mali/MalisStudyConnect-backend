53b892845893559f81213eb772d436fbee6b0615
mouZz-mali <moussacoulibaly06092004@gmail.com>
2025-10-15 20:42:04 +0000

    last modif

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+console.log("📦 MONGO_URI =", process.env.MONGO_URI);
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
